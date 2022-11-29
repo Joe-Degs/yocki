fa02bc1147a598e97a02f4765373d2b52d4b50bb
Joe-Degs <jpedegs8990@gmail.com>
2022-11-29 09:57:06 +0000

    simple hello world example

---
+++ api/hello/hello.go
@@ -0,0 +1,23 @@
+package hello
+
+import (
+	"net/http"
+
+	"github.com/Joe-Degs/yocki/server"
+)
+
+type Hello struct{}
+
+func (Hello) Routes() []*server.Route {
+	return []*server.Route{
+		&server.Route{
+			Path:    "",
+			Methods: []string{"GET"},
+			HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("Hello, World!\n"))
+			},
+		},
+	}
+}
+
+func (Hello) Version() string { return "hello" }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/Joe-Degs/yocki/api/hello"
 	v1 "github.com/Joe-Degs/yocki/api/v1"
 	"github.com/Joe-Degs/yocki/server"
 )
 
 func main() {
 	srv := server.NewServer(":8080")
-	if err := srv.InitRoutes(v1.NewLogService()); err != nil {
-		log.Fatal(err)
+	services := []server.Servicer{v1.NewLogService(), hello.Hello{}}
+	for _, service := range services {
+		if err := srv.InitRoutes(service); err != nil {
+			log.Fatal(err)
+		}
 	}
 	log.Fatal(srv.ListenAndServe())
 }
